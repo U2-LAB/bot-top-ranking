bd254b226185358c478df3be2c040ff5ed64177c
Nikita Efremov <nikefremov.00.008@hotmail.com>
2020-11-04 12:51:04 +0300

    Add unittest for help_functions

---
+++ unittests/conf.py
@@ -0,0 +1,64 @@
+import os
+
+from telebot import types
+
+
+def chat():
+    return types.Chat(id=-1001349185527, type='supergroup')
+
+def user(id=359862454,username='nikefr7'):
+    # 843622237 - Kavahi
+    return types.User(id=id, is_bot=False, first_name='Some User', username=username)
+
+def message(user, chat, text='/start'):
+    params = {'text': text}
+    return types.Message(
+        message_id=27891546, from_user=user, date=None, chat=chat, content_type='text', options=params, json_string=""
+    )
+
+def call(user,data,chat,message):
+    return types.CallbackQuery(
+        id=1,from_user=user,data=data,chat_instance=chat,message=message
+    )
+
+def mock_send_message(id,message_,parse_mode="HTML",reply_markup=None):
+    with open('mock.txt','w+') as f:
+        f.seek(0)
+        f.truncate()
+        print(message_,end='',file=f)
+    return message(user(),chat())
+
+def mock_pin_chat_message(state,message_id,disable_notification):
+    pass
+
+def mock_send_audio(chat_id,audio):
+    with open('song.txt','w+') as f_song:
+        f_song.seek(0)
+        f_song.truncate()
+        print('Audio',file=f_song,end='')
+
+def get_capture(file_name='mock.txt'):
+    with open(file_name, 'r+') as f:
+        capture =''.join(f.readlines())
+    os.remove(file_name)
+    return capture
+
+def mock_download_music_link(link,name):
+
+    with open(f'{name}','wb+') as mp3_test:
+        mp3_test.seek(0)
+        mp3_test.truncate()
+        mp3_test.write(b'Song in mp3')
+
+def mock_state_init():
+    pass
+
+def mock_unpin_chat_message(chat_id):
+    pass
+
+def mock_promote_chat_member(id,user_id,can_delete_messages=True):
+    pass
+
+def mock_set_chat_administrator_custom_title(chat_id,user_id,name):
+    pass
+
---
+++ unittests/test_help_functions.py
@@ -0,0 +1,93 @@
+import unittest
+import os
+import time
+
+import telebot
+from telebot import types
+
+from bot_top_ranking import help_functions
+from bot_top_ranking.handlers import state
+
+from .conf import (
+    message,
+    chat,
+    user,
+    mock_download_music_link,
+    mock_send_audio,
+    get_capture,
+)
+from unittest import skip
+from unittest.mock import patch, Mock
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class TestHelpFunctions(unittest.TestCase):
+    def setUp(self):
+        self.bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
+        self.User = user()
+        self.Chat = chat()
+        self.Message = message(self.User,self.Chat)
+    
+    def test_gen_markup(self):
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.row_width = 1
+        keyboard.add(types.InlineKeyboardButton("Help", callback_data="help"),)
+        self.assertEqual(help_functions.gen_markup().__dict__, keyboard.__dict__)
+
+    @patch('bot_top_ranking.help_functions._download_music_link',side_effect=mock_download_music_link)
+    def test_download_music_link(self, mock_dwnld):
+        link = 'https://zaycev.net/musicset/dl/65f2858273af4c1747dcf013b4402a88/21847355.json'
+        self.assertIsNone(help_functions._download_music_link(link,'author - title.mp3'))
+        capture = get_capture('author - title.mp3')
+        expected_output = 'Song in mp3'
+        self.assertEqual(capture,expected_output)
+
+    def test_incorrect_music_link(self):
+        link = 'https://vk.com'
+        self.assertIsNone(help_functions._download_music_link(link,'author - title.mp3'))
+
+    @patch('bot_top_ranking.help_functions._download_music_link',side_effect=mock_download_music_link)
+    def test_upload_song(self, mock_dwnld):
+        song = {
+            "title" : "title",
+            "author" : "author",
+            "link" : 'https://zaycev.net/musicset/dl/65f2858273af4c1747dcf013b4402a88/21847355.json',
+            "mark" : 5,
+            "pos" : 1
+        }        
+        mock_bot = Mock()
+        mock_bot.send_audio = mock_send_audio
+        self.assertIsNone(help_functions.upload_song(song,mock_bot,state))
+        capture_upload = get_capture('song.txt')
+        self.assertEqual(capture_upload,'Audio')
+
+    def test_create_top(self):
+        songs = [
+            {
+                "title" : "title",
+                "author" : "author",
+                "link" : 'https://zaycev.net/musicset/dl/65f2858273af4c1747dcf013b4402a88/21847355.json',
+                "mark" : 1,
+                "pos" : 1
+            },
+            {
+                "title" : "title",
+                "author" : "author",
+                "link" : 'https://zaycev.net/musicset/dl/65f2858273af4c1747dcf013b4402a88/21847355.json',
+                "mark" : 2,
+                "pos" : 1
+            }   ,
+            {
+                "title" : "title",
+                "author" : "author",
+                "link" : 'https://zaycev.net/musicset/dl/65f2858273af4c1747dcf013b4402a88/21847355.json',
+                "mark" : 3,
+                "pos" : 1
+            }
+        ]
+        self.assertEqual(help_functions.create_top(songs),songs[::-1])
+
+    def test_create_empty_top(self):
+        songs = []
+        self.assertEqual(help_functions.create_top(songs),songs[::-1])
